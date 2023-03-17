70cc5ac825cfb756ea7333238afaced16e0330f1
DarwinCamahalan <darpax1@gmail.com>
2023-03-17 15:21:06 +0800

    Successfully Added Billing Statement Files - 95% Complete!

--- website/components/BillingStatement/BillingStatement.js
+++ website/components/BillingStatement/BillingStatement.js
@@ -7,6 +7,7 @@ const BillingStatement = () => {
   const [files, setFiles] = useState([])
   const [searchTerm, setSearchTerm] = useState('')
   const [selectedMonth, setSelectedMonth] = useState('')
+  const [selectedOption, setSelectedOption] = useState('')
 
   useEffect(() => {
     async function fetchFiles() {
@@ -31,11 +32,8 @@ const BillingStatement = () => {
       return false
     }
 
-    // Check if file is from Super Five or Rural Transit
-    if (
-      file.indexOf('Super Five') === -1 &&
-      file.indexOf('Rural Transit') === -1
-    ) {
+    // Check if file is from the selected option
+    if (selectedOption !== '' && file.indexOf(selectedOption) === -1) {
       return false
     }
 
@@ -48,42 +46,59 @@ const BillingStatement = () => {
         <p>Billing Statements Files</p>
       </div>
       <div className={styles.billingMenu}>
-        <input
-          type="month"
-          value={selectedMonth}
-          onChange={(e) => setSelectedMonth(e.target.value)}
-        />
-        <div className={styles.search}>
+        <div className={styles.selectContainer}>
+          <label htmlFor="company">Sort By</label>
+          <select
+            id="company"
+            value={selectedOption}
+            onChange={(e) => setSelectedOption(e.target.value)}
+          >
+            <option value="">-</option>
+            <option value="Rural Transit">Rural Transit</option>
+            <option value="Super Five">Super Five</option>
+          </select>
+        </div>
+        <div className={styles.sortMenu}>
           <input
-            type="text"
-            id="text-input"
-            value={searchTerm}
-            onChange={(e) => setSearchTerm(e.target.value)}
+            type="month"
+            value={selectedMonth}
+            onChange={(e) => setSelectedMonth(e.target.value)}
           />
-          <IoSearchOutline className={styles.searchIcon} />
+          <div className={styles.search}>
+            <input
+              type="text"
+              id="text-input"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+            <IoSearchOutline className={styles.searchIcon} />
+          </div>
         </div>
       </div>
-      <div className={styles.content}>
-        {filteredFiles.map((url) => {
-          if (!url) {
-            // Skip over undefined or null URLs
-            return null
-          }
-          return (
-            <a
-              href={url}
-              target="_blank"
-              rel="noopener noreferrer"
-              key={url}
-              className={styles.files}
-            >
-              <div className={styles.logo}>
-                <AiOutlineFilePdf />
-              </div>
-              <p>{url.split('/').pop()}</p>
-            </a>
-          )
-        })}
+
+      <div className={styles.container}>
+        <div className={styles.content}>
+          {filteredFiles.map((url) => {
+            if (!url) {
+              // Skip over undefined or null URLs
+              return null
+            }
+            return (
+              <a
+                href={url}
+                target="_blank"
+                rel="noopener noreferrer"
+                key={url}
+                className={styles.files}
+              >
+                <div className={styles.logo}>
+                  <AiOutlineFilePdf />
+                </div>
+                <p>{url.split('/').pop()}</p>
+              </a>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
